e630b74b9d8f11d4566fcaaad266f55cb6b26bf9
Nate Cramer <natecramer@gmail.com>
2022-10-30 16:40:36 -0400

    Add some map data and drawing

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let tileData = [];
+
 const tileSize = 32;
 
 let x = 320;
@@ -17,9 +19,14 @@ let input = {
 
 let inputMap = {
     "ArrowRight": 'right',
-    'ArrowLeft': 'left',
-    'ArrowDown': 'down',
-    'ArrowUp': 'up'
+    'ArrowLeft':  'left',
+    'ArrowDown':  'down',
+    'ArrowUp':    'up',
+
+    "d": 'right',
+    'a': 'left',
+    's': 'down',
+    'w': 'up'
 };
 
 
@@ -58,10 +65,70 @@ document.addEventListener('keyup', (event) => {
 const updateDelay = 16.7; // 60 updates per second (1000 / 60)
 let updateCounter = 0;
 
+
 // init
 updatePosition();
 let myTimeout = setTimeout(update, updateDelay);
+const MAP_WIDTH = 255;
+
+function setMapFromTileData(start_col, start_row) {
+    const mapDiv = document.querySelector('#map');
+    let idx = 0;
+    for (let row = start_row; row < start_row+20; row++) {
+        for (let col = 0; col < start_col+20; col++) {
+            idx = tileData[Math.floor(row * MAP_WIDTH) + col];
+            let tile = document.createElement('div');
+            tile.classList.add('tile');
+            let tileCol = idx % 18;
+            let tileRow = Math.floor(idx / 18);
+            let x = (tileCol * 34) + 2; // (col * 17) + 1
+            let y = (tileRow * 34) + 2;
+            tile.style['background-position'] = `top -${y}px left -${x}px`;
+            // tile.textContent = `${idx}`;
+            // tile.style.color = '#fff';
+            // tile.style['font-size'] = '8pt';
+            mapDiv.appendChild(tile);
+        }
+    }
+    // for (let i = 0; i < tileData.length; i++) {
+    //     let idx = tileData[i];
+    //     let tile = document.createElement('div');
+    //     tile.classList.add('tile');
+    //     let col = idx % 18;
+    //     let row = Math.floor(idx / 18);
+    //     let x = (col * 34) + 2; // (col * 17) + 1
+    //     let y = (row * 34) + 2;
+    //     tile.style['background-position'] = `top -${y}px left -${x}px`;
+    //     tile.textContent = `${idx}`;
+    //     tile.style.color = '#fff';
+    //     tile.style['font-size'] = '8pt';
+    //     mapDiv.appendChild(tile);
+    // }
+}
+
+var xhr = new XMLHttpRequest();
+xhr.onreadystatechange = function() {
+    if (xhr.readyState == 4 && this.status == 200) {
+        tilesStrs = this.responseText.split(' ');
+        // tileData = [];
+        let n = 0;
+        tilesStrs.forEach(e => {
+            n = parseInt(e);
+            // if (!isNaN(n))
+            if (n == undefined) {
+                console.log(`${e} was undefined`);
+            }
+            tileData.push(n);
+        });
+        // console.log(tileData);
+        setMapFromTileData(0, 0);
+    }
+};
+xhr.open('GET', 'tiledata.txt');
+xhr.send();
+
 // document.querySelector('#music').play();
+
 
 // update ()
 function update() {
